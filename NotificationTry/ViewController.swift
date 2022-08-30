be0b1f41e97f719b923505f8dfd7f11a8062ee9e
buuraak77 <brkylmz8@hotmail.com>
2022-08-30 20:03:28 +0300

    Initial Commit

---
+++ NotificationTry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotificationTry
+//
+//  Created by Burak Yılmaz on 30.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
